deee309f2a29f038570a7ab226643202d8921663
priyanshu1430 <priyanshugpt143@gmail.com>
2023-12-20 20:35:38 +0530

    do2

---
+++ ARRAY/MEDIUM/degree90rot.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+	int m=mat[0].size();
+	for (int i = 0; i < n; i++)
+	{
+   		 for (int j = 0; j < i; j++) 
+		{
+        swap(mat[i][j], mat[j][i]);
+   		}
+    }
+	for (int i = 0; i < n; i++) 
+	{
+    	reverse(mat[i].begin(), mat[i].end());
+    }
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+            cout<< mat[i][j];
+        }
+    }
+    }
+};
